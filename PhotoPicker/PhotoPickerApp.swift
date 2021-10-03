d4ac58374840a336e970f2916793559402ed287b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 03:19:07 +0700

    Create the project

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Danh Tu on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
